ddea51ab478f239a367b95d51d298a1880eb4951
AlexiaMaria11 <oprtitamariaalexia@gmail.com>
2025-03-18 23:35:07 +0200

    Transform Arbori Generalizati

---
+++ TransArbFiu/TransArbFiu/TransArbFiu.h
@@ -0,0 +1,50 @@
+#pragma once
+#include<stdint.h>
+
+#define MAXNODES 100
+
+typedef uint8_t TKey;
+
+typedef struct {
+	uint8_t prim_fiu, frate_dreapta;
+	TKey key;
+}TNode;
+
+typedef uint8_t TNodeRef;
+
+typedef struct {
+	TNode nodes[MAXNODES];
+	uint8_t size; //tree size = number of nodes in the tree	
+}TTree;
+
+typedef struct TNodeB {
+	TKey key;
+	struct TNodeB* left;
+	struct TNodeB* right;
+}TNodeB;
+
+TTree InitTree(TTree a);
+
+TTree InsertNode(TTree a, TNodeRef  prim_fiu, TNodeRef  frate_dreapta, TKey key);
+
+void printArray(TTree a);
+
+void printNode(TNode node, char* message);
+
+TKey nodeKey(TTree a, TNodeRef noderef);
+
+TNode nodeByRef(TTree a, TNodeRef noderef);
+
+TTree deleteNode(TTree a, TNodeRef noderef);
+
+TNodeRef firstChild(TTree a, TNodeRef noderef);
+
+TNodeRef rightSibling(TTree a, TNodeRef noderef);
+
+void preOrder(TTree a);
+void inOrder(TTree a);
+void postOrder(TTree a);
+
+TNodeB* transform(TTree t, TNodeRef noderef);
+
+void freeTree(TNodeB* root);
